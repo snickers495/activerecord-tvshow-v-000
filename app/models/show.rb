4cb569ac8f215446f91b774d26da58644ee37cec
Lisa Lee <snickers495@gmail.com>
2017-10-24 22:38:27 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -12,7 +12,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    Show.where(rating: least_popular_show).first
+    Show.where(rating: lowest_rating).first
   end
 
   def self.popular_shows
