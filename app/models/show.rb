c5dae71681e1c2d0ee4d5f29976741e154e62377
Lisa Lee <snickers495@gmail.com>
2017-10-24 22:37:32 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,19 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     Show.where(rating: highest_rating).first
   end 
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end 
+
+  def self.least_popular_show
+    Show.where(rating: least_popular_show).first 
+  end 
+
+  def self.popular_shows
+    Show.where(rating: highest_rating)
+  end 
+
+  def self.shows_by_alphabetical_order
+    Show.order(title: :desc)
 end
